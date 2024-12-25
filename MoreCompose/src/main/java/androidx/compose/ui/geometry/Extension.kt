b43743ce843c81874da1b894a82f820afff256c1
Chen-Hai Teng <chenhaiteng@gmail.com>
2024-12-25 15:33:30 +0800

    Add offset extension

--- MoreCompose/src/main/java/androidx/compose/ui/geometry/Extension.kt
+++ MoreCompose/src/main/java/androidx/compose/ui/geometry/Extension.kt
@@ -13,8 +13,13 @@ fun Size.fitSquare(center: Offset, insetRatio: Float = 0f): Rect {
     return Rect(center, radius)
 }
 
+// Path Extension
 fun Path.moveTo(offset: Offset) = moveTo(offset.x, offset.y)
 
 fun Path.lineTo(offset: Offset) = lineTo(offset.x, offset.y)
 
 fun Path.cubicTo(control1: Offset, control2: Offset, point: Offset) = cubicTo(control1.x, control1.y, control2.x, control2.y, point.x, point.y)
+
+// Offset Extension
+fun Offset.minus(x: Float = 0f, y: Float = 0f): Offset = minus(Offset(x, y))
+fun Offset.plus(x: Float = 0f, y: Float = 0f): Offset = plus(Offset(x, y))
