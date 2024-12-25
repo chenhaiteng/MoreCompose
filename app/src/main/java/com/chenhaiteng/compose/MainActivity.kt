97deb56f39f604059d7467933c9c17550b73cd60
Chen-Hai Teng <chenhaiteng@gmail.com>
2024-12-25 15:33:57 +0800

    update sample app

--- app/src/main/java/com/chenhaiteng/compose/MainActivity.kt
+++ app/src/main/java/com/chenhaiteng/compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.chenhaiteng.compose
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,16 +13,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.cubicTo
 import androidx.compose.ui.geometry.fitSquare
+import androidx.compose.ui.geometry.lineTo
+import androidx.compose.ui.geometry.minus
+import androidx.compose.ui.geometry.moveTo
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawBlueprint
 import androidx.compose.ui.graphics.drawscope.drawRect
+import androidx.compose.ui.graphics.drawscope.drawText
 import androidx.compose.ui.graphics.rotatedSweepGradient
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.fullBoundingBox
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.minus
+import androidx.compose.ui.unit.sp
 import com.chenhaiteng.compose.ui.theme.MoreComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,35 +52,54 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+fun Brush.Companion.colorRing(degree: Float) = Brush.rotatedSweepGradient(colorStops = arrayOf(
+    0.0f to Color.Red,
+    0.333f to Color.Green,
+    0.666f to Color.Blue,
+    1.0f to Color.Red
+), degree = degree)
+
 @Composable
-fun MoreComposeGreeting(modifier: Modifier) {
+fun MoreComposeGreeting(modifier: Modifier, greeting: String) {
+    val measurer = rememberTextMeasurer()
+    val fontSize = 30.sp
+    val layout = measurer.measure(greeting, style = TextStyle.Default.copy(fontSize = fontSize, color = Color.Cyan))
     MoreComposeCanvas(modifier) {
         val rect = size.fitSquare(center)
+        val textTopLeft = rect.topLeft.plus(Offset(x = 50f, y = 50f))
         drawRect(rect.deflate(20.0f), Color.Red, style = Stroke(width = 5.0f))
         drawRect(rect.deflate(40.0f), Color.White, style = Stroke(width = 10.0f))
+        drawText(layout, topLeft = textTopLeft, bluePrintColor = Color.White)
+        drawText(layout, topLeft = textTopLeft.plus(Offset.Zero.copy(y = fontSize.toPx() + 10f)), brush = Brush.colorRing(degree = 0f))
+        val p = Path().apply {
+            moveTo(rect.center)
+            lineTo(rect.bottomRight)
+            cubicTo(rect.bottomRight.minus(y = 160f), rect.bottomLeft.minus(y = 160f), rect.bottomLeft)
+            lineTo(rect.center)
+        }
+        drawPath(path = p, color = Color.Yellow, style = Stroke(width = 2.0f))
     }
 }
 
+val blueBrush = Brush.rotatedSweepGradient(colorStops = arrayOf(
+    0.0f to Color.Blue,
+    0.1f to Color.Blue.copy(alpha = 0.5f),
+    0.2f to Color.Blue,
+    0.4f to Color.Blue.copy(alpha = 0.5f),
+    0.5f to Color.Blue,
+    0.6f to Color.Blue,
+    0.8f to Color.Blue.copy(alpha = 0.5f),
+    0.9f to Color.Blue,
+), degree = 0f)
+
 @Composable
 fun MoreComposeCanvas(modifier: Modifier, drawScope: DrawScope.()->Unit) {
-    Canvas(modifier = modifier.background(Brush.rotatedSweepGradient(colorStops = arrayOf(
-        0.0f to Color.Blue,
-        0.1f to Color.Blue.copy(alpha = 0.5f),
-        0.2f to Color.Blue,
-        0.4f to Color.Blue.copy(alpha = 0.5f),
-        0.5f to Color.Blue,
-        0.6f to Color.Blue,
-        0.8f to Color.Blue.copy(alpha = 0.5f),
-        0.9f to Color.Blue,
-    ), degree = 0f)), drawScope)
+    Canvas(modifier = modifier.background(blueBrush), drawScope)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MoreComposeCanvas(Modifier.fillMaxSize()) {
-        val rect = size.fitSquare(center)
-        drawRect(rect, Color.Red, style = Stroke(width = 5.0f))
-        drawRect(rect.deflate(40.0f), Color.White, style = Stroke(width = 10.0f))
-    }
+    MoreComposeGreeting(Modifier.fillMaxSize(), greeting = "Hello!! MoreCompose!")
 }
