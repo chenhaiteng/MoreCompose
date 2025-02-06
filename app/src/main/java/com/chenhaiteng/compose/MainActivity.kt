90f777951ec7c5d27e5298bf9cb88a95ab9cdb2c
Chen-Hai Teng <chenhaiteng@gmail.com>
2025-02-06 14:11:16 +0800

    optimize import

--- app/src/main/java/com/chenhaiteng/compose/MainActivity.kt
+++ app/src/main/java/com/chenhaiteng/compose/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.chenhaiteng.compose
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.MoreIconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.cubicTo
 import androidx.compose.ui.geometry.fitSquare
@@ -27,17 +25,13 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.drawBlueprint
 import androidx.compose.ui.graphics.drawscope.drawRect
 import androidx.compose.ui.graphics.drawscope.drawText
 import androidx.compose.ui.graphics.rotatedSweepGradient
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.fullBoundingBox
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.minus
 import androidx.compose.ui.unit.sp
 import com.chenhaiteng.compose.ui.theme.MoreComposeTheme
 
